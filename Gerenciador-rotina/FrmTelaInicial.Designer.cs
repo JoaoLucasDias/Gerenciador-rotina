666289f2905191798ed69ca5103d437fc6321dfe
Joao Lucas Dias <jld150109@gmail.com>
2025-09-30 12:40:14 -0300

    aaaa

--- Gerenciador-rotina/FrmTelaInicial.Designer.cs
+++ Gerenciador-rotina/FrmTelaInicial.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmTelaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1924, 1061);
+            this.Name = "FrmTelaInicial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmTelaInicial";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmTelaInicial_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
